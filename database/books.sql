71346169068e459abdd521cdbfa6398b58c8348a
Tyler <tylersayvetz@gmail.com>
2020-01-23 11:36:00 -0800

    finish insert functionality

--- database/books.sql
+++ database/books.sql
@@ -3,9 +3,10 @@ CREATE TABLE books(
   id SERIAL PRIMARY KEY,
   author VARCHAR(255),
   title VARCHAR(255),
-  isbn VARCHAR(255),
+  isbn VARCHAR(14),
   image_url VARCHAR(255),
   description VARCHAR(255),
+  fullDescription VARCHAR,
   bookshelf VARCHAR(255)
 );
 
